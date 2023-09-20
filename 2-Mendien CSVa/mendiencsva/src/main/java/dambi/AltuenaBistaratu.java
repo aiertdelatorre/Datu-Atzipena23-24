db5248b405045a5b15e54edcbc8c2b1c84a49bde
aiertdelatorre <delatorre.aiert@uni.eus>
2023-09-20 14:03:02 +0200

    java update

--- 2-Mendien CSVa/mendiencsva/src/main/java/dambi/AltuenaBistaratu.java
+++ 2-Mendien CSVa/mendiencsva/src/main/java/dambi/AltuenaBistaratu.java
@@ -1,5 +1,39 @@
 package dambi;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+
 public class AltuenaBistaratu {
+    public static void main(String[] args) throws IOException{
     
+        BufferedReader inputStream = null;
+        PrintWriter outputStream = null;
+
+        inputStream = new BufferedReader(new FileReader("Mendiak.csv"));
+        outputStream = new PrintWriter(new FileWriter("CopyLines.txt"));
+
+        String l;
+        String[][] arr = new String[100][3];
+        String[] arr2 = {"omdv",Integer.toString(0),"all"};
+
+        arr[0] = inputStream.readLine().split(";");
+
+        for(int a =1;(l = inputStream.readLine()) != null;a++) {
+            arr[a] = l.split(";");
+            if(Integer.parseInt(arr[a][1]) > Integer.parseInt(arr2[1])){
+                arr2[0] = arr[a][0];               
+                arr2[1] = arr[a][1];
+                arr2[2] = arr[a][2];
+            }
+        }
+        System.out.println(arr2[0] + " " + arr2[1] + " " + arr2[2]);
+        outputStream.close();
+        inputStream.close();
+    }
 }
+
+
+
--- 2-Mendien CSVa/mendiencsva/src/main/java/dambi/MendiakEsportatu.java
+++ 2-Mendien CSVa/mendiencsva/src/main/java/dambi/MendiakEsportatu.java
@@ -1,5 +1,69 @@
+
 package dambi;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+
 public class MendiakEsportatu {
+    public static void main(String[] args) throws IOException{
     
+        BufferedReader inputStream = null;
+        PrintWriter outputStream = null;
+
+        inputStream = new BufferedReader(new FileReader("Mendiak.csv"));
+        outputStream = new PrintWriter(new FileWriter("CopyLines.txt"));
+
+        String l;
+        String[][] mendidanak = new String[100][3];
+        String[][] gipuzkoa = new String[5][3];       
+        String[][] bizkaia = new String[4][3];
+        String[][] araba = new String[8][3];
+
+        mendidanak[0] = inputStream.readLine().split(";");
+
+        for(int a =1;(l = inputStream.readLine()) != null;a++) {
+            int g = 0;
+            int b= 0;
+            int ar = 0;
+            mendidanak[a] = l.split(";");
+            if(mendidanak[a][2] == "Gipuzkoa"){
+                gipuzkoa[g] = mendidanak[a];
+                g++;
+            }else if(mendidanak[a][2] == "Bizkaia"){
+                bizkaia[b] = mendidanak[a];
+                b++;
+            }else{
+                araba[ar] = mendidanak[a];
+            }
+        }
+
+        for(int i = 0;i<bizkaia.length;i++){
+            System.out.print(bizkaia[i][0]);
+            System.out.print(bizkaia[i][1]);
+            System.out.println(bizkaia[i][2]);
+        }
+
+        for(int i = 0;i<gipuzkoa.length;i++){
+            System.out.print(bizkaia[i][0]);
+            System.out.print(bizkaia[i][1]);
+            System.out.println(bizkaia[i][2]);
+        }
+
+        for(int i = 0;i<gipuzkoa.length;i++){
+            System.out.print(bizkaia[i][0]);
+            System.out.print(bizkaia[i][1]);
+            System.out.println(bizkaia[i][2]);
+        }
+
+
+
+        outputStream.close();
+        inputStream.close();
+    }
 }
+
+
+
